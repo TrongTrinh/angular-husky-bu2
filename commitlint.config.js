d239d092cad0703b418d11c5bfcbe9c1ddce5b9b
TrongTrinh58 <trinhcheo54b@gmail.com>
2024-10-24 09:28:46 +0700

    feat(commitlint): set scope is required

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ['@commitlint/config-angular'] };
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    // 'scope-enum': [2, 'always', [/^ID-\d+$/]],
+    // 'scope-case': [2, 'always', 'pascal-case'],
+    'scope-empty': [2, 'never'],
+  },
+};
